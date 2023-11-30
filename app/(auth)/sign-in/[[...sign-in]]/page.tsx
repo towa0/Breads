4c710995a0a6551af883fd89b08be286463569e0
Kyran Janssen <janssenkyran999@gmail.com>
2023-11-30 13:34:26 +0400

    fixed positioning sign-in and register page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center min-h-screen">
       <SignIn />
       <div className="bg-dark-2 p-7 w-auto sm:w-[20vw] mt-10 rounded-lg ml-7 mr-7 sm:mr-0">
         <p className="text-base-semibold text-light-1">Don't trust me?</p>
@@ -11,6 +11,6 @@ export default function Page() {
           Password: aVerySecurePassWord123?!
         </p>
       </div>
-    </>
+    </div>
   );
 }
